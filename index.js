38c38903fc7e50e7d3999ce26304e1c5140082d6
DylanWard14 <dylan.ward@iinet.net.au>
2021-04-04 14:32:29 +1000

    Added playground

--- index.js
+++ index.js
@@ -59,7 +59,12 @@ const resolvers = {
   },
 };
 
-const server = new ApolloServer({ typeDefs, resolvers });
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  introspection: true,
+  playground: true,
+});
 
 server.listen({ port: process.env.PORT || 4000 }).then(({ url }) => {
   console.log(`Server started at ${url}`);
