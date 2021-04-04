fc71793ecaf5ec6ef57054390d879c0bc321c616
DylanWard14 <dylan.ward@iinet.net.au>
2021-04-04 14:45:05 +1000

    Added better date support

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const { ApolloServer, gql } = require("apollo-server");
+const { GraphQLScalarType } = require("graphql");
+const { Kind } = require("graphql/language");
 
 const typeDefs = gql`
+  scalar Date
+
   enum Status {
     PLAYED
     INTERESTED
@@ -16,7 +20,7 @@ const typeDefs = gql`
   type Game {
     id: ID!
     title: String!
-    releaseDate: String
+    releaseDate: Date
     rating: Int
     developer: Developer
     status: Status
@@ -32,13 +36,13 @@ const games = [
   {
     id: "asdf",
     title: "Call of Duty",
-    releaseDate: "04-11-2020",
+    releaseDate: new Date("04-11-2020"),
     rating: 5,
   },
   {
     id: "asdfaasdf",
     title: "Destiny",
-    releaseDate: "04-11-2020",
+    releaseDate: new Date("04-11-2020"),
     rating: 5,
     developer: {
       id: "asdfaqw",
@@ -57,6 +61,24 @@ const resolvers = {
       return foundGame;
     },
   },
+  Date: new GraphQLScalarType({
+    name: "Date",
+    description: "it's a date",
+    parseValue(value) {
+      // Value from the client
+      return new Date(value);
+    },
+    serialize(value) {
+      // value sent to the client
+      return value.getTime();
+    },
+    parseLiteral(ast) {
+      if (ast.kind === Kind.INT) {
+        return new Date(ast.value);
+      }
+      return null;
+    },
+  }),
 };
 
 const server = new ApolloServer({
