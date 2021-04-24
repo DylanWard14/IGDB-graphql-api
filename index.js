bdde739477d1f56e4278f2d6396746a6e38ed166
DylanWard14 <dylan.ward@iinet.net.au>
2021-04-25 09:59:49 +1000

    Connected to IGDB rest api

--- index.js
+++ index.js
@@ -1,101 +1,42 @@
-const { ApolloServer, gql, PubSub } = require("apollo-server");
-const { GraphQLScalarType } = require("graphql");
-const { Kind } = require("graphql/language");
-const mongoose = require("mongoose");
+const { ApolloServer, gql } = require("apollo-server");
 const dotenv = require("dotenv");
+const GamesAPI = require("./GamesAPI");
 
 dotenv.config();
 
-mongoose.connect(
-  `mongodb+srv://${process.env.MONGODB_LOGIN}:${process.env.MONGODB_PASSWORD}@cluster0.pa2ux.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`,
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  }
-);
-const db = mongoose.connection;
-
-const gameSchema = new mongoose.Schema({
-  title: String,
-  releaseDate: Date,
-  rating: Number,
-  developerId: String,
-  status: String,
-});
-
-const Game = mongoose.model("Game", gameSchema);
-
 // gql`` parses your string into an AST (Abstract Syntax Tree)
 const typeDefs = gql`
-  scalar Date
-
-  enum Status {
-    PLAYED
-    INTERESTED
-    NOT_INTERESTED
-    UNKNOWN
-  }
-
-  type Developer {
+  type Platform {
     id: ID!
-    name: String!
+    name: String
+    slug: String
   }
 
   type Game {
     id: ID!
-    title: String!
-    releaseDate: Date
-    rating: Int
-    developer: Developer
-    status: Status
+    name: String
+    platforms: [Platform]
   }
 
   type Query {
     games: [Game]
     game(id: ID): Game
   }
-
-  input DeveloperInput {
-    id: ID
-  }
-
-  input GameInput {
-    id: ID
-    title: String
-    releaseDate: Date
-    rating: Int
-    status: Status
-    developer: DeveloperInput
-  }
-
-  type Mutation {
-    addGame(game: GameInput): [Game]
-  }
-
-  type Subscription {
-    gameAdded: Game
-  }
 `;
 
-const pubsub = new PubSub();
-const GAME_ADDED = "GAME_ADDED";
-
 const resolvers = {
-  Subscription: {
-    gameAdded: {
-      subscribe: () => pubsub.asyncIterator([GAME_ADDED]),
-    },
-  },
   Query: {
-    games: async () => {
+    games: async (_, __, { dataSources }) => {
       try {
-        return await Game.find();
+        const response = await dataSources.gamesAPI.getGames();
+
+        return response;
       } catch (e) {
         console.log("error: ", e);
         return [];
       }
     },
-    game: async (obj, { id }) => {
+    game: async (_, { id }) => {
       try {
         return await Game.findById(id);
       } catch (e) {
@@ -105,58 +46,38 @@ const resolvers = {
     },
   },
   Game: {
-    developer: (obj, args, context) =>
-      developers.find((developer) => developer.id === obj.developer.id),
-  },
-  Mutation: {
-    addGame: async (obj, { game }, context) => {
+    platforms: async (obj, _, { dataSources }) => {
       try {
-        const newGame = await Game.create({
-          ...game,
-        });
-        pubsub.publish(GAME_ADDED, { gameAdded: newGame });
-
-        return await Game.find();
+        if (!obj.platforms) {
+          return [];
+        }
+        const mappedPlatforms = await Promise.all(
+          obj.platforms.map(async (platformId) => {
+            const [platform] = await dataSources.gamesAPI.getPlatformById(
+              platformId
+            );
+            return platform;
+          })
+        );
+        return mappedPlatforms;
       } catch (e) {
-        console.log("error: ", e);
+        console.log("error:", e);
         return [];
       }
     },
   },
-
-  Date: new GraphQLScalarType({
-    name: "Date",
-    description: "it's a date",
-    parseValue(value) {
-      // Value from the client
-      return new Date(value);
-    },
-    serialize(value) {
-      // value sent to the client
-      return value.getTime();
-    },
-    parseLiteral(ast) {
-      if (ast.kind === Kind.INT) {
-        return new Date(ast.value);
-      }
-      return null;
-    },
-  }),
 };
 
-db.on("error", console.error.bind(console, "connection error:"));
-db.once("open", () => {
-  // we're connected!
-  console.log("Database connected!");
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  dataSources: () => ({
+    gamesAPI: new GamesAPI(),
+  }),
+  introspection: true,
+  playground: true,
+});
 
-  const server = new ApolloServer({
-    typeDefs,
-    resolvers,
-    introspection: true,
-    playground: true,
-  });
-
-  server.listen({ port: process.env.PORT || 4000 }).then(({ url }) => {
-    console.log(`Server started at ${url}`);
-  });
+server.listen({ port: process.env.PORT || 4000 }).then(({ url }) => {
+  console.log(`Server started at ${url}`);
 });
