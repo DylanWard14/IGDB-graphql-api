89dc24486d6d59ef701955f58f107e8d530a8fe2
DylanWard14 <dylan.ward@iinet.net.au>
2021-05-01 09:02:00 +1000

    Setup Typescript and created dataloader for batching platformById requests.

---
+++ src/index.ts
@@ -0,0 +1,66 @@
+import { ApolloServer, gql } from "apollo-server";
+import dotenv from "dotenv";
+import GamesAPI from "./GamesAPI";
+import { Resolvers } from "./resolvers-types";
+
+dotenv.config();
+
+// gql`` parses your string into an AST (Abstract Syntax Tree)
+const typeDefs = gql`
+  type Platform {
+    id: ID!
+    name: String
+    slug: String
+  }
+
+  type Game {
+    id: ID!
+    name: String
+    platforms: [Platform]
+  }
+
+  type Query {
+    games: [Game]
+    game(id: ID): Game
+  }
+`;
+
+const resolvers: Resolvers = {
+  Query: {
+    games: async (_, __, { dataSources }) => {
+      const response = dataSources.gamesAPI.getGames();
+
+      return response;
+    },
+  },
+  Game: {
+    platforms: async (parent, _, { dataSources }) => {
+      if (!parent.platforms) {
+        return [];
+      }
+      const mappedPlatforms = await Promise.all(
+        parent.platforms.map(async (platformId) => {
+          const platform = await dataSources.gamesAPI.getPlatformById(
+            platformId
+          );
+          return platform;
+        })
+      );
+      return mappedPlatforms;
+    },
+  },
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  dataSources: () => ({
+    gamesAPI: new GamesAPI(),
+  }),
+  introspection: true,
+  playground: true,
+});
+
+server.listen({ port: process.env.PORT || 4000 }).then(({ url }) => {
+  console.log(`Server started at ${url}`);
+});
